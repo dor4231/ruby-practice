1d97e5153e24f3b125156bb38d9a9a3bc8ba6a84
dor4231 <dor4231@gmail.com>
2017-07-15 08:42:55 +0300

    Made a violent way to sort the words.

--- sorted_sentence.rb
+++ sorted_sentence.rb
@@ -1,7 +1,11 @@
+
+
 wordsArray = []
+i = 0
 
 puts "Enter some words to sort:"
 puts "(you can add words until you hit Enter twice)"
+
 
 while true
     input = gets.chomp
@@ -11,10 +15,19 @@ while true
         wordsArray.push input
     end
 end
-
-
 wordsArray.each do |word|
     word.capitalize!
 end
-wordsArray.sort!
+
+while wordsArray.length > 0
+    max = wordsArray[0]
+    for word in wordsArray
+        if max > word
+            max = word
+        end
+    end
+    puts max
+    wordsArray.delete(max)
+end
+  
 puts wordsArray
